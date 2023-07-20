5155c8fbe6ccdcf6e5454ee68892602cb87c753f
Sean Dorris <seanc.dorris@gmail.com>
2023-07-20 18:23:10 -0500

    initializing first commit to repo

---
+++ Guessing Game/GuessingGame.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Welcome to Guessing Game!");
+
+var secretNumber = 42;
+
+Welcome();
+void Welcome()
+{
+    Console.WriteLine("Please guess a Number");
+
+    var userGuess = Convert.ToInt32(Console.ReadLine());
+
+    // Console.WriteLine($"You guessed: {userGuess}");
+
+    if (userGuess != secretNumber)
+    {
+        Console.WriteLine("Sorry, that is not the secret number");
+    }
+    else
+    {
+        Console.WriteLine($"Your guess of {userGuess} was correct and matches the secret number of {secretNumber}!");
+    }
+}
